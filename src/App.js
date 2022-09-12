8b74f9e370f2fc495b3c3e897ee9e5f20d91ed79
manikandan M <mahendrasinghmani222@gmail.com>
2022-09-12 11:59:55 +0530

    mode features

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/header/Header";
 import Sidebar from "./components/sidebars/Sidebar";
 import Feed from "./components/feeds/Feeds";
+import {
+  Dialog,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
 const App = () => {
+  const [isOpen, setOpen] = useState(false);
+  useEffect(() => {
+    setOpen(
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      )
+    );
+  });
+
   return (
     <div className="app">
       {/* <h1>lets build linked-In CLONE</h1> */}
@@ -14,6 +29,18 @@ const App = () => {
         <Sidebar />
         {/* Feed */}
         <Feed />
+        <Dialog
+          open={isOpen}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">{"Hi their"}</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Please Use Desktop Mode for Better Experience
+            </DialogContentText>
+          </DialogContent>
+        </Dialog>
         {/* widgets */}
       </div>
     </div>
